e91571e86b822e91c7c35b7864e94666087155fd
redsoxfantom <redsoxfantom@gmail.com>
2017-05-09 19:28:18 -0400

    Implemented InputManager

--- TransactionIngestor/Input/InputManager.cs
+++ TransactionIngestor/Input/InputManager.cs
@@ -5,20 +5,32 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TransactionIngestor.Data;
+using TransactionIngestor.Enums;
+using TransactionIngestor.Input.Ingestors;
 using TransactionIngestor.Interfaces;
 
 namespace TransactionIngestor.Input
 {
     public class InputManager : IDataRecordProducer
     {
-        public InputManager(Func<DataRecord, Tuple<DataRecord, Regex>> RawConverterUpdateNeeded)
-        {
+        private IIngestor ingestor;
+        private RawConverter converter;
 
+        public InputManager(Func<DataRecord, Tuple<DataRecord, Regex>> RawConverterUpdateNeeded, String InputFileName, InputType inputType)
+        {
+            ingestor = IngestorFactory.CreateIngestor(inputType);
+            ingestor.InputFileName = InputFileName;
+
+            converter = new RawConverter(RawConverterUpdateNeeded);
+            converter.Producer = ingestor;
         }
 
         public IEnumerable<DataRecord> GetRecords()
-        {
-            throw new NotImplementedException();
+        { 
+            foreach(var record in converter.GetRecords())
+            {
+                yield return record;
+            }
         }
     }
 }
