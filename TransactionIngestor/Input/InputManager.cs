f7a0c453e2d7708a8d32b0253f6b7080d802f258
tom <redsoxfantom@gmail.com>
2017-05-09 19:06:59 -0400

    Began work on InputManager

---
+++ TransactionIngestor/Input/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TransactionIngestor.Data;
+using TransactionIngestor.Interfaces;
+
+namespace TransactionIngestor.Input
+{
+    public class InputManager : IDataRecordProducer
+    {
+        public InputManager(Func<DataRecord, Tuple<DataRecord, Regex>> RawConverterUpdateNeeded)
+        {
+
+        }
+
+        public IEnumerable<DataRecord> GetRecords()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
