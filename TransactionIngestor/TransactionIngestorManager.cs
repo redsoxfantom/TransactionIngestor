83f04da308e769b8852236bc619e56021bd6e538
redsoxfantom <redsoxfantom@gmail.com>
2017-05-10 05:31:26 -0400

    Implemented transaction manager

--- TransactionIngestor/TransactionIngestorManager.cs
+++ TransactionIngestor/TransactionIngestorManager.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using TransactionIngestor.Data;
+using TransactionIngestor.Enums;
+using TransactionIngestor.Input;
+using TransactionIngestor.Output;
 
 namespace TransactionIngestor
 {
     public class TransactionIngestorManager
     {
-        public TransactionIngestorManager()
-        {
+        InputManager iMgr;
+        OutputManager oMgr;
 
+        public TransactionIngestorManager(Func<DataRecord, Tuple<DataRecord, Regex>> RawConversionNotFound, 
+            InputType inputType, string inputFile, OutputType outputType, string outputFile, bool combine)
+        {
+            iMgr = new InputManager(RawConversionNotFound, inputFile, inputType);
+            oMgr = new OutputManager(outputFile, combine, outputType);
+
+            oMgr.Producer = iMgr;
+        }
+
+        public void Process()
+        {
+            oMgr.Start();
         }
     }
 }
