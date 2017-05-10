355e195fdd245c16fb7151a1636024a9851b1d85
redsoxfantom <redsoxfantom@gmail.com>
2017-05-09 21:23:39 -0400

    Corrected ICombiner interface

--- TransactionIngestor/Output/Combiners/ICombiner.cs
+++ TransactionIngestor/Output/Combiners/ICombiner.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TransactionIngestor.Interfaces;
 
 namespace TransactionIngestor.Combiners
 {
-    public interface ICombiner
+    public interface ICombiner<T> : IDataConsumer<T>, IDataProducer<T>
     {
         String FileToCombineWith { set; }
     }
