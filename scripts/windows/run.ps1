9c50a446fa5bd439fabb440babd5c4a5f6a2a456
Tom English <thomas.f.english@lmco.com>
2019-11-10 13:48:19 -0500

    Completed run powershell script

--- scripts/windows/run.ps1
+++ scripts/windows/run.ps1
@@ -10,6 +10,33 @@ $exe = [IO.Path]::Combine($ExecutableDirectory,'TransactionIngestor.Console.exe'
 $inputfolder = [IO.Path]::Combine($TransactionsDirectory,$year,$month)
 $outputFile = [IO.Path]::Combine($inputfolder,"output.json")
 $firstRun = $true
-$dirs = Get-ChildItem -Directory $inputfolder
 
-& $exe
+Get-ChildItem -Directory $inputfolder | ForEach-Object {
+    $fileType = "$_"
+    $containingFolder = $_.FullName
+    Write-Output "Processing files of type $fileType in folder $containingFolder"
+    
+    Get-ChildItem $containingFolder | ForEach-Object {
+        $filename = $_.FullName
+        Write-Output "Processing $filename..."
+        $combineTag = If ($firstRun) {""} Else {"--combine"}
+        $args = "--input ""{0}"" --inputType {1} --output ""{2}"" {3} --ignoreExtraneousTag IGNORE" -f $filename,$fileType,$outputFile,$combineTag
+        Invoke-Expression "$exe $args"
+        if( -not $? )
+        {
+            Write-Error "Failed to process input file $filename"
+            exit 1
+        }
+        $firstRun = $false
+    }
+}
+
+$args = "--input ""{0}"" --inputType STANDARD_FORMAT_JSON --output ""{1}"" --outputType MONTHLY_TOTALS_CSV --ignoreExtraneousTag IGNORE" -f $outputFile,[IO.Path]::Combine($inputfolder,"month.csv")
+Invoke-Expression "$exe $args"
+if( -not $? )
+{
+    Write-Error "Failed to process input file $filename"
+    exit 1
+}
+
+cmd /c pause
