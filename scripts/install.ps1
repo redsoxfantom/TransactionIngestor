8915152a4567cc421ee5edeade30ec0d03501343
Tom English <thomas.f.english@lmco.com>
2019-11-09 19:59:41 -0500

    Moved output to TransactionsDirectory

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -18,6 +18,16 @@ if( -not (Test-Path $TransactionsDirectory))
     }
 }
 
+if(Test-Path "'$TransactionsDirectory'")
+{
+    Remove-Item "'$TransactionsDirectory'" -Recurse
+    if ( -not $?)
+    {
+        Write-Error "Failed to delete $TransactionsDirectory folder"
+        exit 1
+    }
+}
+
 if(Test-Path "../output")
 {
     Remove-Item "../output" -Recurse
@@ -35,42 +45,49 @@ if ( -not $?)
     exit 1
 }
 
-& dotnet.exe publish -o ../output/TransactionIngestor ../TransactionIngestor.sln 
+& dotnet.exe publish -o "../output/" ../TransactionIngestor.sln 
 if ( -not $?)
 {
     Write-Error "dotnet publish failed"
     exit 1
 }
 
-Copy-Item "./windows/*" -Destination "../output/"
+Copy-Item "./windows/*" -Destination "$TransactionsDirectory"
 if ( -not $?)
 {
-    Write-Error "Script Copy Failed"
+    Write-Error "Windows Script Copy Failed"
     exit 1
 }
 
-Copy-Item "./combined/*" -Destination "../output/"
+Copy-Item "./combined/*" -Destination "$TransactionsDirectory"
 if ( -not $?)
 {
-    Write-Error "Script Copy Failed"
+    Write-Error "Combined Script Copy Failed"
     exit 1
 }
 
-$ValidInputs = & ../output/TransactionIngestor/TransactionIngestor.Install.Support.exe --request "GET_INPUT_TYPES"
+Copy-Item -Path "../output/*" -Destination $TransactionsDirectory/TransactionIngestor -Container
+if ( -not $?)
+{
+    Write-Error "Compiled Output Copy Failed"
+    exit 1
+}
+
+$ValidInputs = & $TransactionsDirectory/TransactionIngestor/TransactionIngestor.Install.Support.exe --request GET_INPUT_TYPES
 if ( -not $?)
 {
     Write-Error "Failed to get input types"
     exit 1
 }
 $ValidInputs = $ValidInputs.Split(', ')
-((Get-Content "../output/README.md" -Raw) -replace '<TRANSACTIONSDIRCTORY>',"$TransactionsDirectory" -replace '<INPUTTYPES>',"$ValidInputs") | Set-Content "../output/README.md"
+((Get-Content "$TransactionsDirectory/README.md" -Raw) -replace '<TRANSACTIONSDIRCTORY>',"$TransactionsDirectory/Inputs" -replace '<INPUTTYPES>',"$ValidInputs") | Set-Content "$TransactionsDirectory/README.md"
 if ( -not $?)
 {
     Write-Error "Failed to replace text in README.md"
     exit 1
 }
 
-((Get-Content "../output/run.ps1" -Raw) -replace '<TRANSACTIONSDIRCTORY>',"$TransactionsDirectory") | Set-Content "../output/run.ps1"
+((Get-Content "$TransactionsDirectory/run.ps1" -Raw) -replace '<TRANSACTIONSDIRCTORY>',"$TransactionsDirectory/Inputs") | Set-Content "$TransactionsDirectory/run.ps1"
 if ( -not $?)
 {
     Write-Error "Failed to replace text in run.ps1"
--- scripts/windows/run.ps1
+++ scripts/windows/run.ps1
@@ -2,4 +2,14 @@ param (
     [String] $TransactionsDirectory = "<TRANSACTIONSDIRCTORY>"
 )
 
+$date = (Get-Date).AddMonths(-1)
+$year = $date.Year
+$month = $date.Month
+$currentdir = (Get-Item -Path ".\").FullName
+$exe = [IO.Path]::Combine($currentdir,'TransactionIngestor','TransactionIngestor.Console.exe')
+$inputfolder = [IO.Path]::Combine($TransactionsDirectory,$year,$month)
+$outputFile = [IO.Path]::Combine($inputfolder,"output.json")
+$firstRun = $true
+$dirs = dir -Directory $inputfolder
+
 & ./TransactionIngestor/TransactionIngestor.Console.exe
