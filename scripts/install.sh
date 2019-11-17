d93c3f9340366925f63eec5a5bfbdc714c960a58
Tom English <redsoxfantom@gmail.com>
2019-11-16 21:31:59 -0500

    Compile solution

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,3 +23,13 @@ if [ -d $InstallDir ]
 then
     try_run "rm -rf $InstallDir"
 fi
+
+CompileDir="../output"
+if [ -d $CompileDir ]
+then
+    try_run "rm -rf $CompileDir"
+fi
+
+SolutionFile="../TransactionIngestor.sln"
+try_run "dotnet restore $SolutionFile"
+try_run "dotnet publish -o $CompileDir $SolutionFile"
