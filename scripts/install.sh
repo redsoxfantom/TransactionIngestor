e843a0a7020003d18612341854fd89dde56c9aa9
Tom English <redsoxfantom@gmail.com>
2019-11-16 21:43:22 -0500

    Copy compiled output and scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,6 +16,7 @@ then
 else
     TransactionsDirectory=$1
 fi
+echo "Creating output directories..."
 try_run "mkdir -p $TransactionsDirectory"
 
 InstallDir=$TransactionsDirectory/TransactionIngestor
@@ -23,6 +24,7 @@ if [ -d $InstallDir ]
 then
     try_run "rm -rf $InstallDir"
 fi
+try_run "mkdir -p $InstallDir"
 
 CompileDir="../output"
 if [ -d $CompileDir ]
@@ -30,6 +32,14 @@ then
     try_run "rm -rf $CompileDir"
 fi
 
+echo "Compiling solution..."
 SolutionFile="../TransactionIngestor.sln"
 try_run "dotnet restore $SolutionFile"
 try_run "dotnet publish -o $CompileDir $SolutionFile"
+
+echo "Copying scripts from linx and combined folders..."
+try_run "cp -Rf ./linux/* $TransactionsDirectory"
+try_run "cp -Rf ./combined/* $TransactionsDirectory"
+
+echo "Copying compiled output to $InstallDir"
+try_run "cp -Rf ../output/* $InstallDir"
