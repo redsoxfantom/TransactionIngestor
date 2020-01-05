efe41ce77057dbe1c174b95d16212757a4d756f8
Tom English <thomas.f.english@lmco.com>
2020-01-05 16:52:30 -0500

    Work on run.py

--- scripts/run.py
+++ scripts/run.py
@@ -1,16 +1,25 @@
 import sys
-from datetime import datetime
+from datetime import datetime,timedelta
 import os
 
 def run():
-    currtime = datetime.now()
-    year = str(currtime.year)
-    month = str(currtime.month)
-    currdir = os.path.dirname(__file__)
+    lastmonth = datetime.now().replace(day=1) - timedelta(days=1)
+    year = str(lastmonth.year)
+    month = str(lastmonth.month)
+    currdir = os.path.abspath(os.path.dirname(__file__))
     exe = os.path.join(currdir,"TransactionIngestor","TransactionIngestor.Console.exe")
     input_folder = os.path.join(currdir,"Inputs",year,month)
     output_file = os.path.join(input_folder,"output.json")
     first_run = True
+
+    for folder in os.listdir(input_folder):
+        containing_folder = os.path.join(input_folder,folder)
+        if os.path.isdir(containing_folder):
+            input_type = folder
+            print(F"Processing files of type {input_type} in folder {containing_folder}")
+            for input_file in os.listdir(containing_folder):
+                input_file = os.path.join(containing_folder,input_file)
+                print(input_file)
 
     return True
 
