9a4789a98801fbe3f4b43c4c59d9de467aa75814
Tom English <redsoxfantom@gmail.com>
2020-02-08 21:22:47 -0500

    Dataconverter converts raw transaction types to parsed

--- transactioningestor/input/dataconverter.py
+++ transactioningestor/input/dataconverter.py
@@ -1,18 +1,45 @@
 from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 import re
+import os
+import json
 
 @dataclass
+@dataclass_json
 class RawConverterData:
     ParsedTransaction: str
-    TransactionRegex: re
+    TransactionRegex: str
 
 class RawConverter:
-    def __init__(self,dataproducer,updateneededmethod):
+    def __init__(self,dataproducer,updateneededmethod,regexfile):
         self._dataproducer = dataproducer
         self._updateneededmethod = updateneededmethod
+        self._regexfile = regexfile
+        if not os.path.isfile(regexfile):
+            self._loadedregexdata = []
+        else:
+            with open(regexfile) as f:
+                self._loadedregexdata = json.load(f)
+                for idx in range(0,len(self._loadedregexdata)):
+                    regex = self._loadedregexdata[idx]['TransactionRegex']
+                    self._loadedregexdata[idx]['TransactionRegex'] = re.compile(regex)
 
     def get_records(self):
         for record in self._dataproducer.get_records():
             if not record.ParsedTransactionType:
-                pass
+                conversionFound = False
+                for loadedconversion in self._loadedregexdata:
+                    if loadedconversion['TransactionRegex'].match(record.RawTransactionType):
+                        record.ParsedTransactionType = loadedconversion['ParsedTransaction']
+                        conversionFound = True
+                        break
+                if not conversionFound:
+                    result = self._updateneededmethod(record)
+                    record.ParsedTransactionType = result[1]
+                    converterdata = RawConverterData()
+                    converterdata.ParsedTransaction = result[1]
+                    converterdata.TransactionRegex = result[0]
+                    self._loadedregexdata.append(converterdata)
+                    with open(self._regexfile, "w") as f:
+                        json.dump(self._loadedregexdata,f)
             yield record
--- transactioningestor/input/tests/__init__.py
+++ transactioningestor/input/tests/__init__.py
@@ -1,22 +1,43 @@
-from transactioningestor.input.dataconverter import RawConverter
+from transactioningestor.input.dataconverter import *
 from transactioningestor.data import DataRecord
+import os
 import unittest
+import tempfile
+import json
 
 class DataProducer:
-    def __init__(self,ParsedTransactionType):
+    def __init__(self,RawTransactionType,ParsedTransactionType):
         self.ParsedTransactionType = ParsedTransactionType
+        self.RawTransactionType = RawTransactionType
     def get_records(self):
-        yield DataRecord(None,self.ParsedTransactionType,None,None)
+        yield DataRecord(self.RawTransactionType,self.ParsedTransactionType,None,None)
 
 class RawConverterTests(unittest.TestCase):
 
     def test_init(self):
-        converter = RawConverter("producer","method")
+        converter = RawConverter("producer","method","file")
         self.assertEqual("producer",converter._dataproducer)
         self.assertEqual("method",converter._updateneededmethod)
+        self.assertEqual(0,len(converter._loadedregexdata))
 
     def test_no_conversion_needed(self):
-        converter = RawConverter(DataProducer("Data"),None)
+        converter = RawConverter(DataProducer("RawData","Data"),None,"file")
         for record in converter.get_records():
             self.assertEqual("Data",record.ParsedTransactionType)
-        
+            self.assertEqual("RawData",record.RawTransactionType)
+
+    def test_regex_conversion(self):
+        converter = RawConverter(DataProducer("DataForTesting",None),"method",os.path.join(os.path.dirname(__file__),"convert.json"))
+        for record in converter.get_records():
+            self.assertEqual("DataForTesting",record.RawTransactionType)
+            self.assertEqual("Custom Parsed Transaction", record.ParsedTransactionType)
+
+    def test_regex_doesnt_match(self):
+        tempdir = tempfile.mkdtemp()
+        testtempfile = os.path.join(tempdir,"temp.json")
+        converter = RawConverter(DataProducer("UNRECOGNIZEDDATA",None),lambda record: ("NEWREGEX","NEWDATA"),testtempfile)
+        for record in converter.get_records():
+            self.assertEqual("UNRECOGNIZEDDATA",record.RawTransactionType)
+            self.assertEqual("NEWDATA",record.ParsedTransactionType)
+        with open(testtempfile) as f:
+            print(f.read())
