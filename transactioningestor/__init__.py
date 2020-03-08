252101f1433a284f952691d90704c9a212ccb081
Tom English <redsoxfantom@gmail.com>
2020-03-08 15:43:47 -0400

    Reverted transactioningestor class
    Was causing unit tests to fail

--- transactioningestor/__init__.py
+++ transactioningestor/__init__.py
@@ -1,7 +1,5 @@
 import os
 from pathlib import Path
-from transactioningestor.input import InputManager
-from transactioningestor.combiners import createcombiner
 
 def get_transaction_dir():
     if "TRANSACTIONS_DIR" in os.environ:
@@ -13,10 +11,7 @@ def get_transaction_dir():
         os.makedirs(homepath)
     return homepath
 
-class TransactionIngestor:
-    def __init__(self,ingestortype,inputfile,updateneededmethod,outputtype,outputfile,filetocombinewith = None,extraneoustag = None):
-        self._inputmanager = InputManager(ingestortype,inputfile,updateneededmethod,extraneoustag)
-        self._combiner = createcombiner(outputtype,self._inputmanager,filetocombinewith)
-
-    def parsetransactions(self):
-        pass
+#class TransactionIngestor:
+#    def __init__(self,ingestortype,inputfile,updateneededmethod,outputtype,outputfile,filetocombinewith = None,extraneoustag = None):
+#        self._inputmanager = InputManager(ingestortype,inputfile,updateneededmethod,extraneoustag)
+#        self._combiner = createcombiner(outputtype,self._inputmanager,filetocombinewith)
