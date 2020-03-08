fc50bce20710ad48f5ea8fe6671d497c10c82e98
Tom English <redsoxfantom@gmail.com>
2020-03-08 15:44:04 -0400

    If no file to combine with, use pass thru

--- transactioningestor/combiners/__init__.py
+++ transactioningestor/combiners/__init__.py
@@ -2,7 +2,7 @@ from transactioningestor.enums import OutputType
 from transactioningestor.combiners.stdfmtjsoncombiner import StandardFormatJsonCombiner
 from transactioningestor.combiners.passthrucombiner import PassThruCombiner
 
-def createcombiner(combinertype,dataproducer,filetocombinewith):
-    if combinertype == OutputType.STANDARD_FORMAT_JSON.name:
+def createcombiner(combinertype,dataproducer,filetocombinewith = None):
+    if combinertype == OutputType.STANDARD_FORMAT_JSON.name and filetocombinewith:
         return StandardFormatJsonCombiner(dataproducer,filetocombinewith)
     return PassThruCombiner(dataproducer)
--- transactioningestor/combiners/tests/__init__.py
+++ transactioningestor/combiners/tests/__init__.py
@@ -1,11 +1,16 @@
 import unittest
 import os
 from unittest.mock import *
+from transactioningestor.enums import OutputType
 from transactioningestor.combiners import createcombiner
 from transactioningestor.combiners.stdfmtjsoncombiner import StandardFormatJsonCombiner
 from transactioningestor.combiners.passthrucombiner import PassThruCombiner
 
 class IngestorTests(unittest.TestCase):
+    def test_create_combiner_no_file(self):
+        combiner = createcombiner(OutputType.STANDARD_FORMAT_JSON.name,"producer")
+        self.assertEquals(combiner.__class__.__name__,"PassThruCombiner")
+
     def test_stdformatcombiner_filenotfound(self):
         os.path.isfile = MagicMock()
         os.path.isfile.return_value = False
