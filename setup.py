f4e7da327fc06826f92a2e1479f606e3c2f6fd50
Tom English <redsoxfantom@gmail.com>
2020-03-08 14:57:00 -0400

    Don't install test modules

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     description="Parse bank transaction history into budget trackers",
     long_description=long_desc,
     long_description_content_type="text/markdown",
-    packages=find_packages(),
+    packages=find_packages(exclude="**/test/*"),
     install_requires=requirements,
     python_requires=">=3.7.3"
 )
