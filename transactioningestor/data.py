7e87825273ad1092a03edc8b878f021fed1c2b05
Tom English <redsoxfantom@gmail.com>
2020-03-08 13:38:41 -0400

    Added method to parse datarecords

--- transactioningestor/data.py
+++ transactioningestor/data.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+import ijson
 
 @dataclass
 class DataRecord:
@@ -7,3 +8,15 @@ class DataRecord:
     ParsedTransactionType: str
     TransactionAmount: float
     TransactionDate: datetime
+
+def get_datarecords_from_file(filename):
+    with open(filename, 'br') as f:
+        for prefix, event, value in ijson.parse(f):
+            if prefix == 'item.TransactionDate':
+                transactiondate = datetime.strptime(value,"%Y-%m-%dT%H:%M:%S")
+            if prefix == 'item.TransactionAmount':
+                transactionamount = value
+            if prefix == 'item.RawTransactionType':
+                rawtransactiontype = value
+            if prefix == 'item.ParsedTransactionType':
+                yield DataRecord(rawtransactiontype,value,transactionamount,transactiondate)
